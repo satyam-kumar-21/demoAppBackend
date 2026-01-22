0e16dea3df74e3c3b8aa8ca179e49261793c7645
Satyam kumar <satyamkumar2716@gmail.com>
2026-01-22 17:10:32 +0530

    change

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins for demo purposes
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
 // Database Connection
@@ -24,6 +28,15 @@ mongoose.connect(MONGO_URI)
 const ScanLog = require('./models/ScanLog');
 
 // Routes
+
+/**
+ * @route   GET /
+ * @desc    Health Check
+ * @access  Public
+ */
+app.get('/', (req, res) => {
+  res.send("Server is Running");
+});
 
 /**
  * @route   POST /api/scan
